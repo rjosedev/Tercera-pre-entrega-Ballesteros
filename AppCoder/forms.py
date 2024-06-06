99aa93f72237fe24fa712685046957f3757886bf
rjosedev <99227430+rjosedev@users.noreply.github.com>
2024-06-05 23:59:57 -0300

    Add files via upload

---
+++ AppCoder/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from models import Proveedor, Dispositivo, Sitio
+
+class ProveedorForm(forms.ModelForm):
+    class Meta:
+        model = Proveedor
+        fields = '__all__'
+
+class DispositivoForm(forms.ModelForm):
+    class Meta:
+        model = Dispositivo
+        fields = '__all__'
+
+class SitioForm(forms.ModelForm):
+    class Meta:
+        model = Sitio
+        fields = '__all__'
+
+class BusquedaForm(forms.Form):
+    termino_busqueda = forms.CharField(max_length=100)
